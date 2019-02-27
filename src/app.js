0ee11aff6eaa7c644f9de4616554abcb513698f8
awmcf <aaronmcfaull@gmail.com>
2019-02-27 13:24:13 +0000

    Wrote prototype for getdata in country model

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
+const Country = require('./models/country.js');
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
+  const country = new Country();
+  country.getData();
 });
--- src/models/country.js
+++ src/models/country.js
@@ -3,13 +3,16 @@ const RequestHelper = require('../helpers/request_helper.js')
 
 const Country = function () {
   this.text = null;
-
 }
 
+Country.prototype.getData = function () {
 
+  const request = new RequestHelper ('https://restcountries.eu/rest/v2/all')
+  request.get((data) => {
+    this.text = data.country;
+    PubSub.publish('Country:country-information-load', this.text)
 
-
-
-
+  });
+};
 
 module.exports = Country;
