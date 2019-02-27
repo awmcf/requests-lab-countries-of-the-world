fa855e725ca33d9d92cdb344a33ab2d05584ba04
awmcf <aaronmcfaull@gmail.com>
2019-02-27 13:17:10 +0000

    Created constant in country model, exported model

---
+++ src/models/country.js
@@ -0,0 +1,15 @@
+const PubSub = require('../helpers/pub_sub.js')
+const RequestHelper = require('../helpers/request_helper.js')
+
+const Country = function () {
+  this.text = null;
+
+}
+
+
+
+
+
+
+
+module.exports = Country;
